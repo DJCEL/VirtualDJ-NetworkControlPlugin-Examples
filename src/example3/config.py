a1427cf9e2daf30e48b2d743783400730f3d8d27
djcel <16272026+DJCEL@users.noreply.github.com>
2026-08-30 07:19:21 +0200

    Delete config.py

--- src/example3/config.py
+++
@@ -1,8 +0,0 @@
-# VirtualDJ - Network Control Plugin (HTTP Server)
-VDJ_NETWORK_CONTROL_HOST = "127.0.0.1" # default: "127.0.0.1"
-VDJ_NETWORK_CONTROL_PORT = 80 # default: 80
-VDJ_NETWORK_CONTROL_PASSWORD = None # default None
-VDJ_NETWORK_CONTROL_TIMEOUT = 10.0  # default: 10 seconds
-
-# VirtualDJ - Process name in tasks manager
-VDJ_PROCESS_NAME = "virtualdj"
